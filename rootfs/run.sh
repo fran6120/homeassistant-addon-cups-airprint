92f50fbfe28d486504582b78a6a70f9d9f54f942
fran6120 <93985959+fran6120@users.noreply.github.com>
2026-04-30 17:58:35 +0200

    Update run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -2,6 +2,16 @@
 
 ulimit -n 524288
 
+# --- ESTO ES LO ÚNICO NUEVO QUE REALMENTE NECESITAS ---
+# 1. Creamos el permiso para que tu red pueda entrar al escáner
+echo "192.168.1.0/24" > /etc/sane.d/saned.conf
+
+# 2. Arrancamos el servidor de escáner en segundo plano (&)
+# Usamos 'root' para no pelearnos con permisos del USB
+/usr/sbin/saned -a root &
+# ------------------------------------------------------
+
+# (Lo que ya estaba) Espera a que el sistema de red esté listo
 until [ -e /var/run/avahi-daemon/socket ]; do
   sleep 1s
 done
@@ -9,9 +19,9 @@ done
 bashio::log.info "Preparing directories"
 if [ ! -d /config/cups ]; then cp -v -R /etc/cups /config; fi
 rm -v -fR /etc/cups
-
 ln -v -s /config/cups /etc/cups
 
-bashio::log.info "Starting CUPS server as CMD from S6"
+bashio::log.info "Starting CUPS server"
 
+# (Lo que ya estaba) Arranca CUPS y se queda bloqueado aquí (es el proceso principal)
 cupsd -f
